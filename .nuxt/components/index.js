5f21842de88fb0a907b895abdbac49a70c90d858
israel <email.israel.br@gmail.com>
2021-02-08 17:30:27 -0400

    remove .nuxt folder

--- .nuxt/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Input } from '../..\\components\\input.vue'
-export { default as LocationCard } from '../..\\components\\locationCard.vue'
-export { default as OfficeCard } from '../..\\components\\officeCard.vue'
-
-export const LazyInput = import('../..\\components\\input.vue' /* webpackChunkName: "components_input" */).then(c => c.default || c)
-export const LazyLocationCard = import('../..\\components\\locationCard.vue' /* webpackChunkName: "components_locationCard" */).then(c => c.default || c)
-export const LazyOfficeCard = import('../..\\components\\officeCard.vue' /* webpackChunkName: "components_officeCard" */).then(c => c.default || c)
